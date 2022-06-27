ac9ad7ce1c22616a7b886e837123f06988f493f3
Shiva Prasad Gurram <gurramshiva05@gmail.com>
2022-06-27 22:44:32 +0530

    Singly Linked List Implementation

--- LinkedList/src/com/shivaprasad/linkedlists/SingleLinkedList.java
+++ LinkedList/src/com/shivaprasad/linkedlists/SingleLinkedList.java
@@ -35,18 +35,33 @@ public class SingleLinkedList {
         obj.printList();
         System.out.println("===================================");
 
-        System.out.println("InsertAtBeginning: ");
+        System.out.println("InsertAtBeginning:7 ");
         obj.insertAtBeginning(7);
         obj.printList();
         System.out.println("===================================");
 
-        System.out.println("InsertAtEnd: ");
+        System.out.println("InsertAtEnd:12 ");
         obj.insertAtEnd(12);
         obj.printList();
         System.out.println("===================================");
 
-        System.out.println("InsertAtGivenPosition: ");
-        obj.insertAtGivenPosition(56,4);
+        System.out.println("InsertAtGivenPosition:3 ");
+        obj.insertAtGivenPosition(56,3);
+        obj.printList();
+        System.out.println("===================================");
+
+        System.out.println("DeleteAtBeginning: ");
+        obj.deleteAtBeginning();
+        obj.printList();
+        System.out.println("===================================");
+
+        System.out.println("DeleteAtEnd: ");
+        obj.deleteAtEnd();
+        obj.printList();
+        System.out.println("===================================");
+
+        System.out.println("DeleteAtGivenPosition:2 ");
+        obj.deleteAtGivenPosition(2);
         obj.printList();
         System.out.println("===================================");
     }
@@ -93,6 +108,11 @@ public class SingleLinkedList {
     void insertAtGivenPosition(int val, int position) {
         if(position < 0 || position > size) return;
         Node node = new Node(val);
+        if(position == 0) {
+            node.next = head;
+            head = node;
+            return;
+        }
         Node currNode = head;
         for(int i=0;i<position-1;i++) {
             currNode = currNode.next;
@@ -101,5 +121,50 @@ public class SingleLinkedList {
         currNode.next = node;
         node.next = temp;
         size++;
+    }
+
+    //Delete at beginning
+    void deleteAtBeginning() {
+        Node node = head;
+        if(node != null) {
+            head = node.next;
+            node.next = null;
+        }
+        size--;
+    }
+
+    //Delete at End
+    void deleteAtEnd() {
+        if(head == null || head.next == null) {
+            head = null;
+            return;
+        }
+        Node currNode = head;
+        Node prevNode = null;
+        while(currNode.next!=null) {
+            prevNode = currNode;
+            currNode = currNode.next;
+        }
+        prevNode.next = currNode.next;
+        size--;
+    }
+
+    //Delete at given position
+    void deleteAtGivenPosition(int position) {
+        if(position < 0 || position > size) return;
+        if(position == 0) {
+            Node node = head;
+            head = node.next;
+            node.next = null;
+            return;
+        }
+        Node currNode = head;
+        Node prevNode = null;
+        for(int i=0;i<position;i++) {
+            prevNode = currNode;
+            currNode = currNode.next;
+        }
+        prevNode.next = currNode.next;
+        size--;
     }
 }
